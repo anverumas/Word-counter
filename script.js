16e45202b3e9aaaabc91240bab27daf093378d5a
anverumas <155238715+anverumas@users.noreply.github.com>
2024-11-01 14:27:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-const element = document.querySelector("input");
-element.addEventListener("keydown", function (event) {
-  console.log(`Key pressed: ${event.key}`);
-  console.log(`User input: ${event.target.value}`);
-  if (event.key === "Enter") {
-    const info = element.value
-    element.innerHTML += "<p>" + element.value + "</p>";
-    element.value = ''
-  }
-});
+async function getlen() {
+  const element = document.querySelector("input");
+  const textField = document.querySelector(".textField");
+  element.addEventListener("keydown", function (event) {
+    console.log(`Key pressed: ${event.key}`);
+    console.log(`User input: ${event.target.value}`);
+    const value = event.target.value
+    if (event.key === "Enter") {
+      text = value.split(' ')
+      len = text.filter(text => text != '', text != ',')
+      textField.innerHTML = "<p>" + len.length + "</p>";
+      element.value = ''
+    }
+  });
+}
+getlen()
