8e3e5ad0149f0a70528966e6ce3825fb6d72b03f
anverumas <155238715+anverumas@users.noreply.github.com>
2024-11-08 15:02:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
-async function getlen() {
-  const element = document.querySelector("input");
-  const textField = document.querySelector(".textField");
-  element.addEventListener("keydown", function (event) {
-    console.log(`Key pressed: ${event.key}`);
-    console.log(`User input: ${event.target.value}`);
-    const value = event.target.value
-    if (event.key === "Enter") {
-      text = value.split(' ')
-      len = text.filter(text => text != '', text != ',')
-      textField.innerHTML = "<p>" + len.length + "</p>";
-      element.value = ''
-    }
-  });
-}
-getlen()
+const element = document.querySelector("input");
+
+const textContent = document.querySelector(".textContent");
+element.addEventListener("keydown", function (event) {
+  console.log(`Key pressed: ${event.key}`);
+  console.log(`User input: ${event.target.value}`);
+  const value = event.target.value
+  if (event.key === "Enter") {
+    text = value.split(' ')
+    len = text.filter(text => text != '')
+    textContent.innerHTML = "<p>" + len.length + "</p>";
+    element.value = ''
+  }
+});
+
+const inputEl = document.querySelector("input");
+
+const optionsEl = document.querySelector(".options");
+const contentEl = document.querySelector(".textContent");
+optionsEl.addEventListener("click", function (event) {
+  const value = inputEl.value
+  text = value.split(' ')
+  len = text.filter(text => text != '')
+  contentEl.innerHTML = "<p>" + len.length + "</p>";
+  inputEl.value = ''
+
+});
