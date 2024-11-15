6308def84a818a553994f7a86515a3fb5780a0f3
anverumas <155238715+anverumas@users.noreply.github.com>
2024-11-15 14:51:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,44 @@
+const url = "https://kool.krister.ee/chat/word_counter";
 const element = document.querySelector("input");
-
-const textContent = document.querySelector(".textContent");
-element.addEventListener("keydown", function (event) {
+const textContent = document.querySelector("#lts");
+const contentSy = document.querySelector(".textContent");
+textContent.addEventListener("click", function (event) {
   console.log(`Key pressed: ${event.key}`);
   console.log(`User input: ${event.target.value}`);
-  const value = event.target.value
-  if (event.key === "Enter") {
-    text = value.split(' ')
-    len = text.filter(text => text != '')
-    textContent.innerHTML = "<p>" + len.length + "</p>";
-    element.value = ''
-  }
+  const value = inputEl.value
+  fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      message: value,
+      num: value.length
+    })
+  });
+  contentEl.innerHTML = "<p>" + value.length + ' symbols' + "</p>";
+  inputEl.value = ''
 });
 
 const inputEl = document.querySelector("input");
 
-const optionsEl = document.querySelector(".options");
+const optionsEl = document.querySelector("#wrds");
 const contentEl = document.querySelector(".textContent");
 optionsEl.addEventListener("click", function (event) {
   const value = inputEl.value
   text = value.split(' ')
   len = text.filter(text => text != '')
-  contentEl.innerHTML = "<p>" + len.length + "</p>";
+  fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      message: value,
+      num: len.length
+    })
+  });
+  contentEl.innerHTML = "<p>" + len.length + ' words' + "</p>";
   inputEl.value = ''
 
 });
